2927bf148a1840910e9d4bc6ee1920d82ef9a275
woidead <abikbest05@gmail.com>
2024-03-20 18:24:00 +0600

    update

--- apps/shop/urls.py
+++ apps/shop/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
-    path('<slug:category_slug>/', views.product_list, name='product_list_by_category/'),
+    path('category/<slug:category_slug>/', views.product_list, name='product_list_by_category'),
     path("<int:id>/<slug:slug>/", views.product_detail, name="product_detail"),
+    path('cart/add/<int:product_id>/', views.cart_add, name="cart_add"),
+    path('cart/remove/<int:product_id>/', views.cart_remove, name="cart_remove"),
+    path('cart/', views.cart_detail, name="cart_detail"),
 ]
--- apps/shop/views.py
+++ apps/shop/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
+from django.views.decorators.http import require_POST
+from .models import Product
+from .cart import Cart
+from .forms import CartAddProductForm
 from .models import Category, Product
 
 def product_list(request, category_slug=None):
@@ -17,3 +21,23 @@ def product_list(request, category_slug=None):
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, category__slug=slug, in_stock=True)
     return render(request, 'shop/product/detail.html', {'product':product})
+
+@require_POST
+def cart_add(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    form = CartAddProductForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(product=product, quantity=cd['quantity'], update_quantity=cd['update'])
+    return redirect('cart_detail')
+
+def cart_remove(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product)
+    return redirect('cart_detail')
+
+def cart_detail(request):
+    cart = Cart(request)
+    return render(request, 'shop/cart/detail.html', {'cart': cart})
