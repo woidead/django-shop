6654dda23959d01111788a0661d996e8cd24e4c9
woidead <abikbest05@gmail.com>
2024-03-20 17:02:54 +0600

    подключены static файлы

--- Myshop/settings.py
+++ Myshop/settings.py
@@ -113,11 +113,17 @@ USE_I18N = True
 
 USE_TZ = True
 
+CART_SESSION_ID = 'cart'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
