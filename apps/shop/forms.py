96bc093081904da59bc341d6a8a55dcd2478c804
woidead <abikbest05@gmail.com>
2024-03-22 16:09:31 +0600

    создана форма OrderCreateForm

--- apps/shop/forms.py
+++ apps/shop/forms.py
@@ -1,5 +1,12 @@
 from django import forms
+from .models import Order
 
 class CartAddProductForm(forms.Form):
     quantity = forms.IntegerField(min_value=1, max_value=9)
     update = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
+
+
+class OrderCreateForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ['first_name', 'last_name', 'email', 'address', 'postal_code', 'city']
