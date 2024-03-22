09fb9569cd59710dab814668886dd80aa1cf5057
woidead <abikbest05@gmail.com>
2024-03-22 17:30:15 +0600

    добавлен маршрут для оформления заказа

--- apps/shop/urls.py
+++ apps/shop/urls.py
@@ -4,9 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
-    path('category/<slug:category_slug>/', views.product_list, name='product_list_by_category'),
-    path("<int:id>/<slug:slug>/", views.product_detail, name="product_detail"),
+    path('category/<slug:category_slug>/', views.product_list, name="product_list_by_category"),
+    path('<int:id>/<slug:slug>/', views.product_detail, name="product_detail"),
     path('cart/add/<int:product_id>/', views.cart_add, name="cart_add"),
     path('cart/remove/<int:product_id>/', views.cart_remove, name="cart_remove"),
     path('cart/', views.cart_detail, name="cart_detail"),
+    path('order/create/', views.order_create, name="order_create"),
 ]
