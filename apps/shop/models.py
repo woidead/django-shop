c3547cd16341422d0e06fbd608c5e9059f0f3303
woidead <abikbest05@gmail.com>
2024-03-22 16:03:18 +0600

    создана модель для заказов

--- apps/shop/models.py
+++ apps/shop/models.py
@@ -23,3 +23,31 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Order(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    address = models.CharField(max_length=255)
+    postal_code = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+    paid = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ("-created",)
+
+    def __str__(self) -> str:
+        return f"Order {self.id}"
+    
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name = 'items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='order_items', on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self) -> str:
+        return str(self.id)
