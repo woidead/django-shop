3efc7816f780d506c66e267cc8932e496018c714
woidead <abikbest05@gmail.com>
2024-03-22 16:25:24 +0600

    создано представление для заказа

--- apps/shop/views.py
+++ apps/shop/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import redirect, render, get_object_or_404
 from django.views.decorators.http import require_POST
-from .models import Product
+from .models import Product, OrderItem
 from .cart import Cart
-from .forms import CartAddProductForm
+from .forms import CartAddProductForm, OrderCreateForm
 from .models import Category, Product
 
 def product_list(request, category_slug=None):
@@ -41,3 +41,20 @@ def cart_remove(request, product_id):
 def cart_detail(request):
     cart = Cart(request)
     return render(request, 'shop/cart/detail.html', {'cart': cart})
+
+
+def order_create(request):
+    cart = Cart(request)
+    if request.method == "POST":
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(order=order, product=item["product"], price=item["price"], quantity=item["quantity"])
+            cart.save()
+            return render(request, "shop/order/created.html", {"order":order})
+    else:
+        form = OrderCreateForm()
+    return render(request, "shop/order/create.html", {"order":order, "form":form})
+
+                
