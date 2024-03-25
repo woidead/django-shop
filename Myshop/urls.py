26cbae5aab680444662e64ad75720e2ac7c2f5d0
woidead <abikbest05@gmail.com>
2024-03-25 15:33:13 +0600

    добавлены ссылки для нового приложения

--- Myshop/urls.py
+++ Myshop/urls.py
@@ -7,4 +7,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.shop.urls')),
+    path('accounts/', include('apps.users.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
